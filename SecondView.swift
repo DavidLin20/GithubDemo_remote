80f8a0502415ecc26b4e15dfd23b6718f8ddbaf6
David Lin <tiweilin20@gamil.com>
2023-06-11 20:21:28 -0700

    Create SecondView

---
+++ SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  gittest
+//
+//  Created by Ti Wei Lin on 6/11/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
